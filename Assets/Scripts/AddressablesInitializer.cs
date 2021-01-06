7e3e446792042fdc324c982527aabbbb2aaa0cf5
Jonathan Day <devjonathanday@gmail.com>
2021-01-06 00:48:32 -0800

    initial commit

---
+++ Assets/Scripts/AddressablesInitializer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using TMPro;
+
+public class AddressablesInitializer : MonoBehaviour
+{
+    public static AddressablesInitializer instance;
+    public TextMeshProUGUI sizeText;
+    public List<string> assetKeys;
+    public TextMeshProUGUI errorLog;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void InitializeAddressables()
+    {
+        try
+        {
+
+            var handle = Addressables.InitializeAsync();
+            handle.Completed += CalculateDownloadSize;
+        }
+        catch (Exception e)
+        {
+            errorLog.text += "\n" + e;
+        }
+    }
+
+    private void CalculateDownloadSize(AsyncOperationHandle<IResourceLocator> obj)
+    {
+        try
+        {
+            var handle = Addressables.GetDownloadSizeAsync(assetKeys.ToArray());
+            handle.Completed += OnCalcSizeCompleted;
+        }
+        catch (Exception e)
+        {
+            errorLog.text += "\n" + e;
+        }
+    }
+
+    public void OnCalcSizeCompleted(AsyncOperationHandle<long> size)
+    {
+        sizeText.text = $"Asset Download Size: {size.Result} bytes";
+    }
+
+    public void DownloadAssets()
+    {
+        try
+        {
+            var handle = Addressables.DownloadDependenciesAsync(assetKeys.ToArray(), Addressables.MergeMode.None, true);
+            handle.Completed += LogCompletion;
+        }
+        catch (Exception e)
+        {
+            errorLog.text += "\n" + e;
+        }
+    }
+
+    private void LogCompletion(AsyncOperationHandle obj)
+    {
+        sizeText.text = "Finished Downloading Assets!";
+    }
+}
---
+++ Assets/Scripts/GenerateObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using TMPro;
+
+public class GenerateObject : MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+    public int position;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            var handle = Addressables.LoadAssetAsync<GameObject>(AddressablesInitializer.instance.assetKeys[position]);
+            handle.Completed += AssetFinishedLoading;
+        }
+    }
+
+    void AssetFinishedLoading(AsyncOperationHandle<GameObject> obj)
+    {
+        if(obj.Status != AsyncOperationStatus.Failed)
+        {
+            var objToSpawn = obj.Result;
+            Instantiate(objToSpawn, Vector3.zero + (Vector3.right * position), Quaternion.identity);
+            position++;
+        }
+    }
+
+    public void AddString()
+    {
+        AddressablesInitializer.instance.assetKeys.Add(inputField.text);
+    }
+}
